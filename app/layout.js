cfcb358e193db184c502833ede84c83ed191eb69
yamcieirvaine-ui <yamcieirvaine@gmail.com>
2026-09-06 03:30:22 +0800

    Add RootLayout component and metadata for app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Yvainely Order Form",
+  description: "Yvainely Fleur Order Form",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
